9ee99af92c2c705c6f9a484595429938709fc3b6
Flaviostack <62808034+Flaviostack@users.noreply.github.com>
2024-12-27 15:49:22 -0300

    Add files via upload

---
+++ Advinhe a palavra/palavraSecreta.py
@@ -0,0 +1,93 @@
+import random
+import os
+import sys
+import platform
+
+def limpa_tela():
+    sistema = platform.system()
+    if sistema == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def resource_path(relative_path): #usado pra localizar o caminho absoluto da lista de palavras, para o caso de usar o PyInstaller, para criar um exe 
+    try:
+        # O PyInstaller cria uma pasta temporária e armazena o caminho em _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def Main():
+    def reiniciarJogo():
+        reiniciar = input("Deseja jogar novamente? (s/n): ").lower()
+        if reiniciar == "s":
+            Main()
+            return
+        else:
+            print("Obrigado por jogar!")
+            return
+
+    limpa_tela()
+    palavras = carregar_Palavras("Palavras.txt")
+    if not palavras: # Verifica se a lista de palavras está vazia (erro ao carregar arquivo)
+      return # Sai da função Main se o arquivo não foi carregado
+
+    palavraSecreta = random.choice(palavras)
+    letrasAcertadas = ""
+    tentativas = 0
+    limiteTentativas = (len(palavraSecreta)) + 5
+
+    print(f"Dica: a palavra têm {len(palavraSecreta)} letras")
+    print("Algumas palavras podem conter caracteres especiais. acentuação(~`^) e traços(-)")
+    print(f"Você têm {limiteTentativas} tentativas.")
+
+    while True:
+        PedirLetra = input("Chute uma letra: ").lower()
+        """if len(PedirLetra) > 1:
+            print("Digite apenas uma letra por vez!: ")
+            continue"""
+        if PedirLetra in palavraSecreta:
+            letrasAcertadas += PedirLetra
+
+        palavraFormada = ""
+        for Letra in palavraSecreta:
+            if Letra in letrasAcertadas:
+                palavraFormada += Letra
+            else:
+                palavraFormada += "_"
+        tentativas += 1
+        limiteTentativas -= 1
+        print("palavraFormada: " + palavraFormada)
+        print(f"Tentativas restantes: {limiteTentativas}")
+
+        if limiteTentativas == 0:
+            print("Você fez o número máximo de tentativas!")
+            print(f"A palavra era: {palavraSecreta}") # Mostra a palavra secreta quando perde
+            reiniciarJogo()
+            return
+
+        if palavraFormada == palavraSecreta:
+            limpa_tela()
+            print("Parabéns, você acertou a palavra secreta!!!")
+            print("Palavra secreta: " + palavraSecreta)
+            print("Número de tentativas: ", tentativas)
+            reiniciarJogo()
+            return
+
+def carregar_Palavras(Nome_Arquivo):
+    palavras = []
+    caminho_arquivo = resource_path(Nome_Arquivo)
+    try:
+        with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
+            for linha in arquivo:
+                palavra = linha.strip().lower()
+                palavras.append(palavra)
+        return palavras
+    except FileNotFoundError:
+        print(f"Erro: Arquivo '{Nome_Arquivo}' não encontrado.")
+        return [] # Retorna uma lista vazia em caso de erro
+
+if __name__ == "__main__": # Proteção para evitar execução indesejada em importações
+    Main()
