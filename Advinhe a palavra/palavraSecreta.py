fc1143622802db42f104e6b10f72cdee50754942
Flávio Augusto N.S.tack <62808034+Flaviostack@users.noreply.github.com>
2025-01-06 14:44:00 -0300

    Update palavraSecreta.py

--- Advinhe a palavra/palavraSecreta.py
+++ Advinhe a palavra/palavraSecreta.py
@@ -3,13 +3,6 @@ import os
 import sys
 import platform
 
-def limpa_tela():
-    sistema = platform.system()
-    if sistema == "Windows":
-        os.system('cls')
-    else:
-        os.system('clear')
-
 def resource_path(relative_path): #usado pra localizar o caminho absoluto da lista de palavras, para o caso de usar o PyInstaller, para criar um exe 
     try:
         # O PyInstaller cria uma pasta temporária e armazena o caminho em _MEIPASS
@@ -19,18 +12,39 @@ def resource_path(relative_path): #usado pra localizar o caminho absoluto da lis
 
     return os.path.join(base_path, relative_path)
 
-def Main():
+def importar_Palavras(Nome_Arquivo):
+    palavras = []
+    caminho_arquivo = resource_path(Nome_Arquivo)
+    try:
+        with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
+            for linha in arquivo:
+                palavra = linha.strip().lower()
+                palavras.append(palavra)
+        return palavras
+    except FileNotFoundError:
+        print(f"Erro: Arquivo '{Nome_Arquivo}' não encontrado.")
+        return [] # Retorna uma lista vazia em caso de erro    
+
+def limpa_tela():
+    sistema = platform.system()
+    if sistema == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def jogo():
     def reiniciarJogo():
         reiniciar = input("Deseja jogar novamente? (s/n): ").lower()
         if reiniciar == "s":
-            Main()
+            jogo()
             return
         else:
             print("Obrigado por jogar!")
             return
-
     limpa_tela()
-    palavras = carregar_Palavras("Palavras.txt")
+    
+    palavras = importar_Palavras("Palavras.txt")
     if not palavras: # Verifica se a lista de palavras está vazia (erro ao carregar arquivo)
       return # Sai da função Main se o arquivo não foi carregado
 
@@ -75,19 +89,4 @@ def Main():
             print("Número de tentativas: ", tentativas)
             reiniciarJogo()
             return
-
-def carregar_Palavras(Nome_Arquivo):
-    palavras = []
-    caminho_arquivo = resource_path(Nome_Arquivo)
-    try:
-        with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
-            for linha in arquivo:
-                palavra = linha.strip().lower()
-                palavras.append(palavra)
-        return palavras
-    except FileNotFoundError:
-        print(f"Erro: Arquivo '{Nome_Arquivo}' não encontrado.")
-        return [] # Retorna uma lista vazia em caso de erro
-
-if __name__ == "__main__": # Proteção para evitar execução indesejada em importações
-    Main()
+jogo()
